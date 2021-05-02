7be325b2a5b313affc7c292fcb03659ab34116e0
Omprakash <2019297@iiitdmj.ac.in>
2021-05-02 19:59:36 +0530

    added js file

---
+++ script.js
@@ -0,0 +1,38 @@
+
+    
+
+      // document.getElementById("clock").innerHTML = h + " : " + m + " : " + s; /* adding time to the div */
+      // t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+    
+      var a,b,c,d;
+
+       function myFunction1(val)
+       {
+         
+       
+            document.getElementById("result").value+=val
+        
+          
+       } 
+
+       function myFunction2(val)
+       {
+         
+       
+        let x = document.getElementById("result").value
+        let y = eval(x)
+        document.getElementById("result").value = y
+        
+          
+       } 
+
+       function clr() {
+        document.getElementById("result").value = ""
+    }
+
+
+      // myFunction1();
+       
+       
+
+      
